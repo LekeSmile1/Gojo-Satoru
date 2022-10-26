2b0f96f2066c2219d73e341744587ba2258bc1cf
LekeSmile1 <116742359+LekeSmile1@users.noreply.github.com>
2022-10-26 16:24:40 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['2348053256030']
+global.premium = ['2348053256030']
+global.ownernomer = '2348053256030'
+global.ownername = '🐦Leke Smile Nexus'
+global.botname = 'Smile Bot 🐦'
+global.footer = '©SmileLeke.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'Smile Bot 🐦'
+global.author = '🐦Leke Smile Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
